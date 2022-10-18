48016a388d243df60f9d273720c215c3aaf24787
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2022-10-18 23:07:10 +0200

    StreamFn – fix event emit, error handling

--- utils/HashStreamFn.ts
+++ utils/HashStreamFn.ts
@@ -1,5 +1,6 @@
-import { Duplex, Readable } from 'stream';
+import { Duplex, EventEmitter, Readable } from 'stream';
 import { createHash, Hash } from 'crypto';
+
 import { StreamFn } from './types';
 
 export function HashStreamFn(algorithm: string): StreamFn<Buffer> {
@@ -7,17 +8,21 @@ export function HashStreamFn(algorithm: string): StreamFn<Buffer> {
 
   const hashInstance: Hash = createHash(algorithm);
 
+  const ee = new EventEmitter();
+
   const stream = Duplex.from(async function* (this: Duplex, source: Readable) {
     for await (const chunk of source) {
       hashInstance.update(chunk);
       yield chunk;
     }
 
-    // TODO: Necessity?
-    process.nextTick(() => this.emit(resultEventName, hashInstance.digest()));
+    ee.emit(resultEventName, hashInstance.digest());
   });
 
-  const result = new Promise<Buffer>((resolve) => stream.once(resultEventName, resolve));
+  const result = new Promise<Buffer>((resolve, reject) => {
+    ee.once(resultEventName, resolve);
+    stream.once('error', reject);
+  })
 
   return { stream, result };
 }
--- utils/SizeStreamFn.ts
+++ utils/SizeStreamFn.ts
@@ -1,4 +1,5 @@
-import { Duplex, Readable } from 'stream';
+import { Duplex, EventEmitter, Readable } from 'stream';
+
 import { StreamFn } from './types';
 
 export function SizeStreamFn(): StreamFn<number> {
@@ -6,17 +7,22 @@ export function SizeStreamFn(): StreamFn<number> {
 
   let byteLength = 0;
 
-  const stream = Duplex.from(async function* (this: Duplex, source: Readable) {
+  const ee = new EventEmitter();
+
+  async function* generator (this: unknown, source: Readable) {
     for await (const chunk of source) {
       byteLength += chunk.byteLength;
       yield chunk;
     }
 
-    // TODO: Necessity?
-    process.nextTick(() => this.emit(resultEventName, byteLength));
-  });
+    ee.emit(resultEventName, byteLength);
+  }
 
-  const result = new Promise<number>((resolve) => stream.once(resultEventName, resolve));
+  const stream = Duplex.from(generator);
+  const result = new Promise<number>((resolve, reject) => {
+    ee.once(resultEventName, resolve);
+    stream.once('error', reject);
+  });
 
   return { stream, result };
 }
